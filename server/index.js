6de0335c94714db39ee75d9050ab8a937faa6234
aws753951 <a0983204766@gmail.com>
2023-06-19 23:05:48 +0800

    setting server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Altas has been connected.");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.use(express.json()); //解析JSON格式的請求，並把其附加到req.body
+app.use(express.urlencoded({ extended: true })); //解析post當中的參數(x-www-form-urlencoded)，附加到req.body
+app.use(helmet()); // 保護req用的
+app.use(morgan("common")); //後續可針對使用者搜尋做log
+
+app.get("/check", (req, res) => {
+  return res.send("OK");
+});
+
+app.listen(6969, () => {
+  console.log("Port 6969 is running.");
+});
